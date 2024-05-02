05aec3ed7c3e75a9cbe50619d50cb0b42973b850
张tiger <zzhang0518@gmail.com>
2024-05-03 02:01:49 +0800

    FixedPoint is working on. The voltage control model is ok

---
+++ SinLut.m
@@ -0,0 +1,16 @@
+clc;
+clear;
+TableSize = 2^10;
+sinTable = zeros(2,TableSize);
+cosTable = zeros(2,TableSize);
+for i = 1:TableSize
+    Angle_rad = i*pi/2/TableSize;
+    sinTable(1,i) = sin(Angle_rad);
+    sinTable(2,i) = Angle_rad;
+    cosTable(1,i) = cos(Angle_rad);
+    cosTable(2,i) = Angle_rad;
+end
+plot(sinTable);
+
+
+
